234e914470c6acf7e1366228ad5c2b41ae50d667
cogden14 <cogden14@gmail.com>
2018-11-07 22:46:37 -0600

    Add project template

---
+++ scope/lists/lib/lists.ex
@@ -0,0 +1,18 @@
+defmodule Scope do
+  @moduledoc """
+  Documentation for Scope.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Scope.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ scope/scope/test/scope_test.exs
@@ -0,0 +1,8 @@
+defmodule ScopeTest do
+  use ExUnit.Case
+  doctest Scope
+
+  test "greets the world" do
+    assert Scope.hello() == :world
+  end
+end
