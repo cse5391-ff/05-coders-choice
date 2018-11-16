cc89a084e62da2b68b07402b02bcfb5d633362d2
Austin Chen <chenas@smu.edu>
2018-11-15 23:48:50 -0600

    Crafting elixir code for pulling NBA player stat API data

--- hooptastic/lib/hooptastic/players/players.ex
+++ hooptastic/lib/hooptastic/players/players.ex
@@ -1,3 +1,22 @@
 defmodule Hooptastic.Players do
-# endpoint to get: https://stats.nba.com/stats/commonplayerinfo/?PlayerID=2000
+
+  def get_player_stats(url) do
+    case HTTPoison.get(url) do
+      {:ok, %{status_code: 200, body: body}} ->
+        Poison.decode!(body)
+
+      {:ok, %{status_code: 404}} ->
+        IO.inspect("IT FAILED DUDE")
+
+      {:error, %{reason: reason}} ->
+        IO.inspect(reason)
+    end
+  end
+
+  def get_all_player_data(num) do
+    for x <- 0..num, do: get_player_stats("https://stats.nba.com/stats/commonplayerinfo/?PlayerID=#{x}")
+  end
+  test = Hooptastic.Players.get_all_player_data(50)
+
+
 end
