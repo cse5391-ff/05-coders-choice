8dca563512668cbe473a2bdeb8a61fbd16a49302
Austin Chen <chenas@smu.edu>
2018-11-15 23:54:22 -0600

    API requests still timeout but getting closer

--- hooptastic/lib/hooptastic/players/players.ex
+++ hooptastic/lib/hooptastic/players/players.ex
@@ -3,7 +3,8 @@ defmodule Hooptastic.Players do
   def get_player_stats(url) do
     case HTTPoison.get(url) do
       {:ok, %{status_code: 200, body: body}} ->
-        Poison.decode!(body)
+        req = Poison.decode!(body)
+        IO.inspect(req)
 
       {:ok, %{status_code: 404}} ->
         IO.inspect("IT FAILED DUDE")
@@ -14,9 +15,9 @@ defmodule Hooptastic.Players do
   end
 
   def get_all_player_data(num) do
-    for x <- 0..num, do: get_player_stats("https://stats.nba.com/stats/commonplayerinfo/?PlayerID=#{x}")
+    for x <- 2..num, do: get_player_stats("https://stats.nba.com/stats/commonplayerinfo/?PlayerID=#{x}")
   end
-  test = Hooptastic.Players.get_all_player_data(50)
-
+  #test = Hooptastic.Players.get_all_player_data(50)
+  # get_player_stats(https://stats.nba.com/stats/commonplayerinfo/?PlayerID=2)
 
 end
