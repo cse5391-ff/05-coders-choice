bdf5188479f94a4f36478d822ac5a0ebafe0a628
Austin Chen <chenas@smu.edu>
2018-11-25 21:57:13 -0600

    Fixed add-move function but broken print board function now

--- sudoku/lib/sudoku/game.ex
+++ sudoku/lib/sudoku/game.ex
@@ -1,8 +1,21 @@
 defmodule Sudoku.Game do
+  # To-Do:
+  # Generate board function
+  # Check board function
+  #   contains: check column, check row, check group
+
+  # put move
+
+  # print board
 
   def new_game() do
     # To-do:
     instantiate_board()
+    #generate_puzzle()
+  end
+
+  def generate_puzzle() do
+
   end
 
   def add_move(board, coord, move) do
@@ -22,29 +35,38 @@ defmodule Sudoku.Game do
     [r, c] = coord
     |> String.capitalize()
     |> String.codepoints()
-    #|> Enum.each(&(&1 |> String.to_atom))
-    IO.puts(r)
-    IO.puts(c)
-    Map.replace!(Map.get(board, r), c, move)
+    row = r |> String.to_atom
+    Kernel.put_in(board[row][c], move)
+  end
+
+  def try_move(_board, _string, _move, false, true) do
+    IO.puts("Invalid coordinate placement!")
+    :invalid_coord
+  end
+
+  def try_move(_board, _string, _move, true, false) do
+    IO.puts("Invalid value!")
+    :invalid_value
   end
 
   def try_move(_board, _string, _move, _valid_coord, _valid_move) do
-    IO.puts("Invalid move!")
+    IO.puts("Invalid coordinate placement and value!")
+    :invalid_both
   end
 
   def instantiate_board() do
     _board = %{
-      "A" => %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-      "B" => %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-      "C" => %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+      A: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+      B: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+      C: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
 
-      "D" => %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-      "E" => %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-      "F" => %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+      D: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+      E: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+      F: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
 
-      "G" => %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-      "H" => %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-      "I" => %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+      G: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+      H: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+      I: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
     }
   end
 
@@ -54,7 +76,7 @@ defmodule Sudoku.Game do
       "
            1 2 3   4 5 6   7 8 9
          -------------------------
-       A | #{board["A"]["1"]} #{board["A"]["2"]} #{board["A"]["3"]} | #{board["A"]["4"]} #{board["A"]["5"]} #{board["A"]["6"]} | #{board["A"]["7"]} #{board["A"]["8"]} #{board["A"]["9"]} |
+       A | #{board[:A]["1"]} #{board[:A]["2"]} #{board[:A]["3"]} | #{board[A]["4"]} #{board[A]["5"]} #{board[A]["6"]} | #{board[A]["7"]} #{board[A]["8"]} #{board[A]["9"]} |
        B | #{board["B"]["1"]} #{board["B"]["2"]} #{board["B"]["3"]} | #{board["B"]["4"]} #{board["B"]["5"]} #{board["B"]["6"]} | #{board["B"]["7"]} #{board["B"]["8"]} #{board["B"]["9"]} |
        C | #{board["C"]["1"]} #{board["C"]["2"]} #{board["C"]["3"]} | #{board["C"]["4"]} #{board["C"]["5"]} #{board["C"]["6"]} | #{board["C"]["7"]} #{board["C"]["8"]} #{board["C"]["9"]} |
          --------+-------+--------
@@ -72,10 +94,6 @@ defmodule Sudoku.Game do
     )
   end
 
-  # Generate board
-  # check board
-  # put move
-  # print board
 
 
 
