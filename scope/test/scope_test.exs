7d0cd9dd86bc44ccfd2758ab1ceb5242e4bfd5de
cogden14 <cogden14@gmail.com>
2018-12-01 10:52:47 -0600

    add tests:

--- scope/test/scope_test.exs
+++ scope/test/scope_test.exs
@@ -5,4 +5,20 @@ defmodule ScopeTest do
   test "greets the world" do
     assert Scope.hello() == :world
   end
+
+  test "creates chat server" do
+    assert Manager.create_channel(:new, "Test Channel") == {:ok, pid}
+  end
+
+  test "sends message to client" do
+    assert Server.send_message(:urgent, "test message") == {ok, payload}
+  end
+
+  test "lists messages" do
+    assert Server.list_messages() != null
+  end
+
+  test "deletes supervisor", sup do
+    assert stop_supervised(sup) == :ok
+  end
 end
