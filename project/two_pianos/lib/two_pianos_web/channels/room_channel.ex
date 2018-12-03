098323bd483224100f5c8ce98b4773f4762ca1c8
Cragsdale1996 <cragsdale@smu.edu>
2018-12-02 23:23:19 -0600

    set up room channel with relevant functions

--- project/two_pianos/lib/two_pianos_web/channels/room_channel.ex
+++ project/two_pianos/lib/two_pianos_web/channels/room_channel.ex
@@ -2,6 +2,16 @@ defmodule TwoPianos.RoomChannel do
 
   use TwoPianosWeb, :channel
 
+  def join("room:" <> room_id, _, socket) do
+    {:ok, socket}
+  end
 
+  def handle_in("keys_pressed", keys, socket) do
+    socket |> broadcast!("press", keys)
+  end
+
+  def handle_in("keys_released", keys, socket) do
+    socket |> broadcast!("release", keys)
+  end
 
 end
