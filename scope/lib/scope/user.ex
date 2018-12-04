c22c3dbcb86f23d822b87302f92413945b2bd921
cogden14 <cogden14@gmail.com>
2018-12-04 09:59:53 -0600

    Add User module for server subscription and message viewing

---
+++ scope/lib/scope/user.ex
@@ -0,0 +1,28 @@
+defmodule User do
+  @servers
+  defmodule Read do
+    defstruct servers: [], messages: []
+  end
+
+  def request(:all, topic) do
+    {:ok, :messges}
+  end
+
+  def request(:unread, read, topic) do
+    {:error, :notimplemented}
+  end
+
+  # Adjust the priority of the channel [:active, :quiet]
+  def set_priority(priority, server) do
+    {:error, :not_implemented, priority}
+  end
+
+  def view_msgs(server) do
+    {:error, :not_implemented, server}
+  end
+
+  # Request to join a server, optional key to grant access
+  def join(server, _key) do
+    {:error, :not_implemented}
+  end
+end
