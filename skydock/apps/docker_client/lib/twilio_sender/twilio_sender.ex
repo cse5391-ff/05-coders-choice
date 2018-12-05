1105482bdabed35aa62c8e751d166dacb50658c9
Lucas Hansen <lchansen97@gmail.com>
2018-12-05 08:29:56 -0600

    added generic send_message function

--- skydock/apps/docker_client/lib/twilio_sender/twilio_sender.ex
+++ skydock/apps/docker_client/lib/twilio_sender/twilio_sender.ex
@@ -5,6 +5,10 @@ defmodule DockerClient.TwilioSender do
     GenServer.start_link(__MODULE__, nil, params)
   end
 
+  def send_message(sms_params, to, from, body) do
+    GenServer.cast(DockerClient.TwilioSender, {:send_message, to, from, body})
+  end
+
   def send_response(sms_params, message) do
     GenServer.cast(DockerClient.TwilioSender, {:send_response, sms_params, message})
   end
@@ -13,6 +17,13 @@ defmodule DockerClient.TwilioSender do
   @impl true
   def init(state) do
     {:ok, state}
+  end
+
+  @impl true
+  def handle_cast({:send_message,  to, from, body}, _state) do
+    response = ExTwilio.Message.create(to: to, from: from, body: body)
+    IO.inspect(response)
+    {:noreply, nil}
   end
 
   @impl true
