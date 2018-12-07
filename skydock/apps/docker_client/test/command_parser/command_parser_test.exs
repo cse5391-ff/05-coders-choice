c9e86fc5a613d9751799087cae69fb5e5014cc6f
Lucas Hansen <lchansen97@gmail.com>
2018-12-07 04:16:18 -0600

    add test skeleton

---
+++ skydock/apps/docker_client/test/command_parser/command_parser_test.exs
@@ -0,0 +1,7 @@
+
+defmodule DockerClientTest.CommandParser do
+  use ExUnit.Case
+  doctest DockerClient.CommandParser
+
+
+end
--- skydock/apps/docker_client/test/docker/docker_test.exs
+++ skydock/apps/docker_client/test/docker/docker_test.exs
@@ -1,7 +1,10 @@
 
-defmodule DockerClientTest do
+defmodule DockerClientTest.Docker do
   use ExUnit.Case
-  doctest DockerClient
+  doctest DockerClient.Docker
+
+  # Module is tricky to test because it directly communicates with the live docker sock (if it's even running)
+  # Tests on this module should be done at developer or user discretion
 
   test "fetches container list (ps)" do
     assert DockerClient.Docker.get_containers() |> elem(0) == :ok
