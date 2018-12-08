00cf35e119f012e31f2bfd7b6cbd9fddb25fe459
Cragsdale1996 <cragsdale@smu.edu>
2018-12-08 12:36:54 -0600

    FE development (lobby page, piano)

--- project/two_pianos/assets/js/app.js
+++ project/two_pianos/assets/js/app.js
@@ -19,3 +19,29 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+build_piano = function(octaves) {
+
+    piano_html = ''
+
+    notes = {
+        white: ['c', 'd', 'e', 'f', 'g', 'a', 'b'],
+        black: ['cs', 'ds', 'fs', 'gs', 'as']
+    }
+
+    for(octave = 1; octave < octaves; octave++){
+
+        // White keys
+        for(k in notes.white) {
+            piano_html += `<div class="white-key ${k}-${octave}"></div>`
+        }
+
+        // Black keys
+        for(k in notes.black) {
+            piano_html += `<div class="black-key ${k}-${octave}"></div>`
+        }
+
+    }
+
+    document.getElementById('piano-container').innerHTML = piano_html
+}
