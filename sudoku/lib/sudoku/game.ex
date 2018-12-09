8380c919a4456c96b234e0efce816bbc541de134
Austin Chen <chenas@smu.edu>
2018-12-09 13:41:28 -0600

    Refactored game board structure

--- sudoku/lib/sudoku/game.ex
+++ sudoku/lib/sudoku/game.ex
@@ -32,30 +32,38 @@ defmodule Sudoku.Game do
   # 3. Add concurrency
   # 4. Create GUI for webapp
 
-  @rows   'ABCDEFGHI'
-  @columns '123456789'
+    @row_mapping %{:A=>0, :B=>1, :C=>2, :D=>3, :E=>4, :F=>5, :G=>6, :H=>7, :I=>8}
+    @rows         ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+    @columns      ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
   def generate_puzzle(board) do
-    for row <- @rows do
-       for col <- @columns do
-        r = List.to_string([row])
-        c = List.to_string([col])
-        # IO.puts(r)
-        # IO.puts(c)
-        board = add_move(board, Enum.join([r, c]), Enum.random(1..9))
-       end
+    instantiate_board()
+  end
+
+  def check_valid_move(board, coord, move) do
+  end
+
+  def check_column(board, coord, move) do
+
+  end
+
+  def check_row(board, coord, move) do
+    [r, c] = coord
+    |> String.capitalize()
+    |> String.codepoints()
+    row = r |> String.to_atom
+
+    list_row = []
+
+    for col <- @columns do
+      list_row.append(board[row][col])
+      IO.puts()
     end
+
+    IO.puts("#{row}: #{board[row]["1"]} #{board[row]["2"]} #{board[row]["3"]} #{board[row]["4"]} #{board[row]["5"]} #{board[row]["6"]} #{board[row]["7"]} #{board[row]["8"]} #{board[row]["9"]}")
   end
 
-  def check_column(board, val) do
-
-  end
-
-  def check_row(board, val) do
-
-  end
-
-  def check_group(board, val) do
+  def check_group(board, coord, move) do
 
   end
 
@@ -63,6 +71,7 @@ defmodule Sudoku.Game do
   def check_win(board) do
 
   end
+
 
   # To-do with add move:
   # once check_group, check_row, and check_column is implemented, integrate this into validity checking
@@ -105,42 +114,68 @@ defmodule Sudoku.Game do
   end
 
   def instantiate_board() do
-    _board = %{
-      A: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-      B: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-      C: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+    for _ <- 0..9, do: (for _ <- 0..8, do: 0)
+  end
 
-      D: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-      E: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-      F: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+  # _board = %{
+    #   A: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+    #   B: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+    #   C: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
 
-      G: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-      H: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-      I: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-    }
+    #   D: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+    #   E: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+    #   F: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+
+    #   G: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+    #   H: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+    #   I: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
+    # }
+
+  def get_val(board, coord) do
+    [r, c] = coord |> coord_split
+
+    board
+    |> Enum.at(@row_mapping[r])
+    |> Enum.at(c)
+  end
+
+  def coord_split(coord) do
+    [r, c] = coord
+    |> String.capitalize()
+    |> String.codepoints()
+    #row = r |> String.to_atom
+    [r |> String.to_atom, (c |> String.to_integer) - 1]
   end
 
   # Utility function for printing board on command line (to see if my stuff is working)
   def print_board(board) do
+    # IO.puts("     1 2 3   4 5 6   7 8 9  ")
+    # IO.puts("   -------------------------")
+    # for row <- @rows do
+    #   for col <- @columns do
+    #     IO.write( board |> get_val( Enum.join([row, col]) ) )
+    #   end
+    #   IO.puts("")
+    # end
+
+    # This is the ugly way of doing it woops
     IO.puts(
       "
            1 2 3   4 5 6   7 8 9
          -------------------------
-       A | #{board[:A]["1"]} #{board[:A]["2"]} #{board[:A]["3"]} | #{board[:A]["4"]} #{board[:A]["5"]} #{board[:A]["6"]} | #{board[:A]["7"]} #{board[:A]["8"]} #{board[:A]["9"]} |
-       B | #{board[:B]["1"]} #{board[:B]["2"]} #{board[:B]["3"]} | #{board[:B]["4"]} #{board[:B]["5"]} #{board[:B]["6"]} | #{board[:B]["7"]} #{board[:B]["8"]} #{board[:B]["9"]} |
-       C | #{board[:C]["1"]} #{board[:C]["2"]} #{board[:C]["3"]} | #{board[:C]["4"]} #{board[:C]["5"]} #{board[:C]["6"]} | #{board[:C]["7"]} #{board[:C]["8"]} #{board[:C]["9"]} |
+       A | #{get_val(board, "A1")} #{get_val(board, "A2")} #{get_val(board, "A3")} | #{get_val(board, "A4")} #{get_val(board, "A5")} #{get_val(board, "A6")} | #{get_val(board, "A7")} #{get_val(board, "A8")} #{get_val(board, "A9")} |
+       B | #{get_val(board, "B1")} #{get_val(board, "B2")} #{get_val(board, "B3")} | #{get_val(board, "B4")} #{get_val(board, "B5")} #{get_val(board, "B6")} | #{get_val(board, "B7")} #{get_val(board, "B8")} #{get_val(board, "B9")} |
+       C | #{get_val(board, "C1")} #{get_val(board, "C2")} #{get_val(board, "C3")} | #{get_val(board, "C4")} #{get_val(board, "C5")} #{get_val(board, "C6")} | #{get_val(board, "C7")} #{get_val(board, "C8")} #{get_val(board, "C9")} |
          --------+-------+--------
-       D | #{board[:D]["1"]} #{board[:D]["2"]} #{board[:D]["3"]} | #{board[:D]["4"]} #{board[:D]["5"]} #{board[:D]["6"]} | #{board[:D]["7"]} #{board[:D]["8"]} #{board[:D]["9"]} |
-       E | #{board[:E]["1"]} #{board[:E]["2"]} #{board[:E]["3"]} | #{board[:E]["4"]} #{board[:E]["5"]} #{board[:E]["6"]} | #{board[:E]["7"]} #{board[:E]["8"]} #{board[:E]["9"]} |
-       F | #{board[:F]["1"]} #{board[:F]["2"]} #{board[:F]["3"]} | #{board[:F]["4"]} #{board[:F]["5"]} #{board[:F]["6"]} | #{board[:F]["7"]} #{board[:F]["8"]} #{board[:F]["9"]} |
+       D | #{get_val(board, "D1")} #{get_val(board, "D2")} #{get_val(board, "D3")} | #{get_val(board, "D4")} #{get_val(board, "D5")} #{get_val(board, "D6")} | #{get_val(board, "D7")} #{get_val(board, "D8")} #{get_val(board, "D9")} |
+       E | #{get_val(board, "E1")} #{get_val(board, "E2")} #{get_val(board, "E3")} | #{get_val(board, "E4")} #{get_val(board, "E5")} #{get_val(board, "E6")} | #{get_val(board, "E7")} #{get_val(board, "E8")} #{get_val(board, "E9")} |
+       F | #{get_val(board, "F1")} #{get_val(board, "F2")} #{get_val(board, "F3")} | #{get_val(board, "F4")} #{get_val(board, "F5")} #{get_val(board, "F6")} | #{get_val(board, "F7")} #{get_val(board, "F8")} #{get_val(board, "F9")} |
          --------+-------+--------
-       G | #{board[:G]["1"]} #{board[:G]["2"]} #{board[:G]["3"]} | #{board[:G]["4"]} #{board[:G]["5"]} #{board[:G]["6"]} | #{board[:G]["7"]} #{board[:G]["8"]} #{board[:G]["9"]} |
-       H | #{board[:H]["1"]} #{board[:H]["2"]} #{board[:H]["3"]} | #{board[:H]["4"]} #{board[:H]["5"]} #{board[:H]["6"]} | #{board[:H]["7"]} #{board[:H]["8"]} #{board[:H]["9"]} |
-       I | #{board[:I]["1"]} #{board[:I]["2"]} #{board[:I]["3"]} | #{board[:I]["4"]} #{board[:I]["5"]} #{board[:I]["6"]} | #{board[:I]["7"]} #{board[:I]["8"]} #{board[:I]["9"]} |
+       G | #{get_val(board, "G1")} #{get_val(board, "G2")} #{get_val(board, "G3")} | #{get_val(board, "G4")} #{get_val(board, "G5")} #{get_val(board, "G6")} | #{get_val(board, "G7")} #{get_val(board, "G8")} #{get_val(board, "G9")} |
+       H | #{get_val(board, "H1")} #{get_val(board, "H2")} #{get_val(board, "H3")} | #{get_val(board, "H4")} #{get_val(board, "H5")} #{get_val(board, "H6")} | #{get_val(board, "H7")} #{get_val(board, "H8")} #{get_val(board, "H9")} |
+       I | #{get_val(board, "I1")} #{get_val(board, "I2")} #{get_val(board, "I3")} | #{get_val(board, "I4")} #{get_val(board, "I5")} #{get_val(board, "I6")} | #{get_val(board, "I7")} #{get_val(board, "I8")} #{get_val(board, "I9")} |
          -------------------------
-
       "
-
     )
   end
 
