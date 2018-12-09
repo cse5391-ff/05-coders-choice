1fb83038b1bb2ace3c9673ae1b7814a70a1702a1
cogden14 <cogden14@gmail.com>
2018-12-09 14:03:17 -0600

    Changed ChatServer to Dynamic Supervisor

--- scope/lib/scope/chat_server.ex
+++ scope/lib/scope/chat_server.ex
@@ -1,22 +1,17 @@
 defmodule Scope.ChatServer do
-  use GenServer
+  use DynamicSupervisor
 
   def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, opts)
+    DynamicSupervisor.start_link(__MODULE__, opts)
   end
 
   def init(init_state \\ []) do
-    {:ok, init_state}
+    DynamicSupervisor.init(strategy: :one_for_one)
   end
 
-  def start(:new, topic) do
-    children = [
-      %{id: Channel,
-      start: {Channel, :start_link, [[topic]]}
-    }
-    ]
-    opts = [strategy: :one_for_one]
-    Supervisor.start_link(children, opts)
+  def start_child(:new, topic) do
+    children = {Channel, [topic: topic, _from: "new user"]}
+    DynamicSupervisor.start_child(__MODULE__, children)
 
   end
 
