25afb312412188d0b7096cfaba61ac0dccf30790
Lucas Hansen <lchansen97@gmail.com>
2018-12-09 00:49:36 -0600

    removed junk

--- skydock/apps/docker_client/lib/docker_client.ex
+++ skydock/apps/docker_client/lib/docker_client.ex
@@ -1,18 +1,3 @@
 defmodule DockerClient do
-  @moduledoc """
-  Documentation for DockerClient.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> DockerClient.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  @moduledoc false
 end
