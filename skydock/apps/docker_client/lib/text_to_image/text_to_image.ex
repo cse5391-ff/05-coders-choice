e1b176b097ba3c058751c0dd90681747429bce27
Lucas Hansen <lchansen97@gmail.com>
2018-12-09 00:16:10 -0600

    removed junk

--- skydock/apps/docker_client/lib/text_to_image/text_to_image.ex
+++ skydock/apps/docker_client/lib/text_to_image/text_to_image.ex
@@ -4,10 +4,7 @@ defmodule DockerClient.TextToImage do
     {:ok, file} = File.open "#{file_path}.txt", [:write]
     IO.binwrite file, "text 80,80 \" #{String.replace(text, "\"", "\\\"")} \"  "
     File.close file
-    # res = System.cmd("sh", ["-c", "convert -size 1024x1024 xc:white -pointsize 12 -fill black -draw 'text 80,80 \" #{text} ' #{file_path}"])
-    res = System.cmd("sh", ["-c", "convert -size 1024x1024 xc:white -pointsize 12 -fill black -draw @#{file_path}.txt #{file_path}"])
-    IO.inspect(res)
-    :ok
+    System.cmd("sh", ["-c", "convert -size 1024x1024 xc:white -pointsize 12 -fill black -draw @#{file_path}.txt #{file_path}"])
   end
 
 end
