0f17cd5f02dafb9b5bd1dc759ca71ad6c06287ee
Austin Chen <chenas@smu.edu>
2018-12-09 14:10:02 -0600

    Added value placement fiunction

--- sudoku/lib/sudoku/game.ex
+++ sudoku/lib/sudoku/game.ex
@@ -26,15 +26,9 @@ defmodule Sudoku.Game do
   #      in the board. At each individual square, check to see if there are column collisions or row collisions.
   #   3. Remove elements to complete the "puzzle". There are smart ways to do this and dumb ways to do this.
 
-  # Next steps:
-  # 1. Add validation to the "add move" that can't overwrite original generated values
-  # 2. Perhaps create a game struct that contains the board.
-  # 3. Add concurrency
-  # 4. Create GUI for webapp
-
-    @row_mapping %{:A=>0, :B=>1, :C=>2, :D=>3, :E=>4, :F=>5, :G=>6, :H=>7, :I=>8}
-    @rows         ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
-    @columns      ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  @row_mapping %{:A=>0, :B=>1, :C=>2, :D=>3, :E=>4, :F=>5, :G=>6, :H=>7, :I=>8}
+  @rows         ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+  @columns      ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
   def generate_puzzle(board) do
     instantiate_board()
@@ -48,19 +42,12 @@ defmodule Sudoku.Game do
   end
 
   def check_row(board, coord, move) do
-    [r, c] = coord
-    |> String.capitalize()
-    |> String.codepoints()
-    row = r |> String.to_atom
+    [r, c] = coord |> coord_split
+    r = @row_mapping[r]
 
-    list_row = []
+    IO.inspect(board |> Enum.at(r))
 
-    for col <- @columns do
-      list_row.append(board[row][col])
-      IO.puts()
-    end
-
-    IO.puts("#{row}: #{board[row]["1"]} #{board[row]["2"]} #{board[row]["3"]} #{board[row]["4"]} #{board[row]["5"]} #{board[row]["6"]} #{board[row]["7"]} #{board[row]["8"]} #{board[row]["9"]}")
+    #IO.puts("#{row}: #{board[row]["1"]} #{board[row]["2"]} #{board[row]["3"]} #{board[row]["4"]} #{board[row]["5"]} #{board[row]["6"]} #{board[row]["7"]} #{board[row]["8"]} #{board[row]["9"]}")
   end
 
   def check_group(board, coord, move) do
@@ -72,11 +59,7 @@ defmodule Sudoku.Game do
 
   end
 
-
-  # To-do with add move:
-  # once check_group, check_row, and check_column is implemented, integrate this into validity checking
-  # for add_move.
-
+  # Add a move to the board
   def add_move(board, coord, move) do
     valid_coord = coord
     |> String.match?(~r/[a-iA-I][1-9]$/)
@@ -90,56 +73,51 @@ defmodule Sudoku.Game do
   end
 
   # Regex: // ~r/[A-I][1-9]/
-  def try_move(board, coord, move, true, true) do
-    [r, c] = coord
-    |> String.capitalize()
-    |> String.codepoints()
-    row = r |> String.to_atom
-    Kernel.put_in(board[row][c], move)
+  defp try_move(board, coord, move, true, true) do
+    [r, c] = coord |> coord_split
+
+    Kernel.put_in(board[r][c], move)
   end
 
-  def try_move(_board, _string, _move, false, true) do
+  defp try_move(_board, _string, _move, false, true) do
     IO.puts("Invalid coordinate placement!")
     :invalid_coord
   end
 
-  def try_move(_board, _string, _move, true, false) do
+  defp try_move(_board, _string, _move, true, false) do
     IO.puts("Invalid value!")
     :invalid_value
   end
 
-  def try_move(_board, _string, _move, _valid_coord, _valid_move) do
+  defp try_move(_board, _string, _move, _valid_coord, _valid_move) do
     IO.puts("Invalid coordinate placement and value!")
     :invalid_both
   end
 
+  # Create board structure
   def instantiate_board() do
     for _ <- 0..9, do: (for _ <- 0..8, do: 0)
   end
 
-  # _board = %{
-    #   A: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-    #   B: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-    #   C: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-
-    #   D: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-    #   E: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-    #   F: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-
-    #   G: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-    #   H: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-    #   I: %{"1" => 0, "2" => 0, "3" => 0,   "4" => 0, "5" => 0, "6" => 0,   "7" => 0, "8" => 0, "9" => 0},
-    # }
-
-  def get_val(board, coord) do
+  # Get value at coordinate
+  defp get_val(board, coord) do
     [r, c] = coord |> coord_split
+    r = @row_mapping[r]
 
     board
-    |> Enum.at(@row_mapping[r])
+    |> Enum.at(r)
     |> Enum.at(c)
   end
 
-  def coord_split(coord) do
+  defp put_val(board, coord, val) do
+    [r, c] = coord |> coord_split
+    r = @row_mapping[r]
+
+    board |> List.replace_at(r, board |> Enum.at(r) |> List.replace_at(c, val))
+  end
+
+  # Split coordinate into corresponding atom and integer
+  defp coord_split(coord) do
     [r, c] = coord
     |> String.capitalize()
     |> String.codepoints()
@@ -147,7 +125,7 @@ defmodule Sudoku.Game do
     [r |> String.to_atom, (c |> String.to_integer) - 1]
   end
 
-  # Utility function for printing board on command line (to see if my stuff is working)
+  # Display the board to the user
   def print_board(board) do
     # IO.puts("     1 2 3   4 5 6   7 8 9  ")
     # IO.puts("   -------------------------")
