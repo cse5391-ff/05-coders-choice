2311116d6276a69340b541ece6bb22ba5c0cf6d2
Rui Lin <rachellinrui@gmail.com>
2018-12-10 12:41:10 +0800

    finished server

--- terminal2048/lib/terminal2048/game.ex
+++ terminal2048/lib/terminal2048/game.ex
@@ -12,17 +12,37 @@ defmodule Terminal2048.Game do
     GenServer.call(@me, :new_game)
   end
 
-  def state(game) do
-    GenServer.call(game, :state)
+  def place_number(board) do
+    GenServer.call(@me, {:place_number, board})
   end
 
-  def move(game, direction) do
-    GenServer.call(game, {:move, direction})
+  def make_move(game, direction) do
+    GenServer.call(@me, {:make_move, game, direction})
+  end
+
+  def draw_current_board(game) do
+    GenServer.call(@me, {:draw_current_board, game})
   end
 
   # Implementation
-  def init(_) do
+  def init([]) do
     {:ok, Terminal2048.Board.new_game()}
+  end
+
+  def handle_call(:new_game, _from, state) do
+    {:reply, Terminal2048.Board.new_game(), state}
+  end
+
+  def handle_call({:place_number, board}, _from, state) do
+    {:reply, Terminal2048.Board.place_number(board), state}
+  end
+
+  def handle_call({:make_move, game, direction}, _from, state) do
+    {:reply, Terminal2048.Board.make_move(game, direction), state}
+  end
+
+  def handle_call({:draw_current_board, game}, _from, state) do
+    {:reply, Terminal2048.Board.draw_current_board(game), state}
   end
 
 end
