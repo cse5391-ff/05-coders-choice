8eec2952db4fef70f54603891bbc90c7fd74814b
Cragsdale1996 <cragsdale@smu.edu>
2018-12-09 22:33:46 -0600

    lobby controller now uses IdManager to generate user id

--- project/two_pianos/lib/two_pianos_web/controllers/lobby_controller.ex
+++ project/two_pianos/lib/two_pianos_web/controllers/lobby_controller.ex
@@ -2,18 +2,7 @@ defmodule TwoPianosWeb.LobbyController do
   use TwoPianosWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html", user_id: generate_user_id()
-  end
-
-  # MOVE THIS TO SEPARATE MODULE!! IdGenerator or something who knows
-  def generate_user_id() do
-
-    #Some random method to generate id
-    #Assurance that id is unique
-    #return id
-
-    123
-
+    render conn, "index.html", user_id: IdManager.generate_id(_type = :user, _length = 8)
   end
 
 end
