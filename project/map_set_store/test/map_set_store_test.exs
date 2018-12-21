0be5af63a6f44b80a2fccc0c4a1e04ad34d340e0
Cragsdale1996 <cragsdale@smu.edu>
2018-12-21 13:49:42 -0800

    refactor tests to use setup and fixture

--- project/map_set_store/test/map_set_store_test.exs
+++ project/map_set_store/test/map_set_store_test.exs
@@ -1,46 +1,95 @@
 defmodule MapSetStoreTest do
   use ExUnit.Case
-  doctest MapSetStore
 
-  test "Get returns accurate mapset" do
+  describe "MapSetStore Tests" do
 
-    {:ok, _} = MapSetStore.start(:mss_empty)
-    {:ok, _} = MapSetStore.start(:mss_with_vals, [1,2,3])
+    setup do
 
-    assert MapSet.equal?(MapSetStore.get(:mss_empty),     MapSet.new([]))
-    assert MapSet.equal?(MapSetStore.get(:mss_with_vals), MapSet.new([1, 2, 3]))
+      fixture = [
+        empty:        :mss_empty,
+        with_vals:    :mss_with_vals,
+        initial_vals: [1,3,5,7]
+      ]
 
-  end
+      {:ok, _} = MapSetStore.start(fixture.empty)
+      {:ok, _} = MapSetStore.start(fixture.with_vals, fixture.initial_vals)
 
-  test "Adds single items" do
+      fixture
 
-    {:ok, _} = MapSetStore.start(:mss)
+    end
 
-    MapSetStore.add(:mss, [1])
-    assert MapSet.equal?(MapSetStore.get(:mss), MapSet.new([1]))
+    test "Get returns accurate mapset", fixture do
+      assert MapSet.equal?(MapSetStore.get(fixture.empty),     MapSet.new([]))
+      assert MapSet.equal?(MapSetStore.get(fixture.with_vals), MapSet.new(fixture.initial_vals))
+    end
 
-    MapSetStore.add(:mss, [2])
-    assert MapSet.equal?(MapSetStore.get(:mss), MapSet.new([1, 2]))
+    test "Adds single items", fixture do
 
-    MapSetStore.add(:mss, [3])
-    assert MapSet.equal?(MapSetStore.get(:mss), MapSet.new([1, 2, 3]))
+      mss_name = fixture.empty
 
-  end
+      MapSetStore.add(mss_name, [1])
+      assert MapSet.equal?(MapSetStore.get(:mss), MapSet.new([1]))
 
-  test "Adds all items from enum" do
-    assert MapSetStore.hello() == :world
-  end
+      MapSetStore.add(mss_name, [2])
+      assert MapSet.equal?(MapSetStore.get(:mss), MapSet.new([1,2]))
 
-  test "Removes single items" do
-    assert MapSetStore.hello() == :world
-  end
+      MapSetStore.add(mss_name, [3])
+      assert MapSet.equal?(MapSetStore.get(:mss), MapSet.new([1,2,3]))
 
-  test "Removes all items from enum" do
-    assert MapSetStore.hello() == :world
-  end
+    end
 
-  test "Contains? is accurate" do
-    assert MapSetStore.hello() == :world
+    test "Adds all items from enum", fixture do
+
+      mss_name = fixture.empty
+
+      MapSetStore.add(mss_name, [1,3,5])
+      assert MapSet.equal?(MapSetStore.get(:mss), MapSet.new([1,3,5]))
+
+      MapSetStore.add(mss_name, [5,7,9])
+      assert MapSet.equal?(MapSetStore.get(:mss), MapSet.new([1,3,5,7,9]))
+
+    end
+
+    test "Removes single items", fixture do
+
+      mss_name = fixture.with_vals
+
+      MapSetStore.remove(mss_name, [1])
+      assert MapSet.equal?(MapSetStore.get(:mss), MapSet.new([3,5,7]))
+
+      MapSetStore.remove(mss_name, [5])
+      assert MapSet.equal?(MapSetStore.get(:mss), MapSet.new([3,7]))
+
+      MapSetStore.remove(mss_name, [3])
+      assert MapSet.equal?(MapSetStore.get(:mss), MapSet.new([7]))
+
+      MapSetStore.remove(mss_name, [7])
+      assert MapSet.equal?(MapSetStore.get(:mss), MapSet.new([]))
+
+    end
+
+    test "Removes all items from enum", fixture do
+
+      mss_name = fixture.with_vals
+
+      MapSetStore.remove(mss_name, [1, 5])
+      assert MapSet.equal?(MapSetStore.get(:mss), MapSet.new([3,7]))
+
+      MapSetStore.remove(mss_name, [3, 7, 9])
+      assert MapSet.equal?(MapSetStore.get(:mss), MapSet.new([]))
+
+    end
+
+    test "Contains? is accurate", fixture do
+
+      mss_name = fixture.with_vals
+
+      assert MapSetStore.contains?(mss_name, 3)
+      assert MapSetStore.contains?(mss_name, 7)
+      assert !MapSetStore.contains?(mss_name, 3)
+
+    end
+
   end
 
 end
