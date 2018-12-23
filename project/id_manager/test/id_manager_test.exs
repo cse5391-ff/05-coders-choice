d9d47a0ef5b78e31ca643ec799b4cb6c60737b8f
Cragsdale1996 <cragsdale@smu.edu>
2018-12-23 12:09:38 +0800

    IdManager test setup planning

--- project/id_manager/test/id_manager_test.exs
+++ project/id_manager/test/id_manager_test.exs
@@ -1,8 +1,23 @@
 defmodule IdManagerTest do
   use ExUnit.Case
-  doctest IdManager
 
-  test "greets the world" do
-    assert IdManager.hello() == :world
+  describe "IdManager Tests" do
+
+    setup do
+
+      fixture = [
+
+      ]
+
+      {:ok, _} = MapSetStore.start(:room_ids)
+      {:ok, _} = MapSetStore.start(:user_ids)
+
+    end
+
+    test "greets the world" do
+      assert IdManager.hello() == :world
+    end
+
   end
+
 end
