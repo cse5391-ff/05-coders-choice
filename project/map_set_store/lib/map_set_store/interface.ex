dd68e26846a9552d9e51b33f7d4e63fc9b3a084d
Cragsdale1996 <cragsdale@smu.edu>
2018-12-23 14:21:40 +0800

    add types

--- project/map_set_store/lib/map_set_store/interface.ex
+++ project/map_set_store/lib/map_set_store/interface.ex
@@ -1,14 +1,17 @@
 defmodule MapSetStore.Interface do
   @moduledoc """
-  Directly interacts with the MapSetStore genserver.
+  Directly interacts with the MapSetStore genserver. To keep things simple, we will only accept items and lists of items
+  as arguments. (items = strings, atoms, numbers)
   """
 
   alias MapSetStore.Server
 
+  @type item() :: number() | String.t() | atom()
+
   def     start(name, args),  do: Server |> GenServer.start_link(to_mapset(args), name: name)
   def       get(name),        do: name   |> GenServer.call(:get)
-  def       add(name, args),  do: name   |> GenServer.cast({:add, to_mapset(args)})
-  def    remove(name, args),  do: name   |> GenServer.cast({:remove, to_mapset(args)})
+  def       add(name, args),  do: name   |> GenServer.cast({:add,       to_mapset(args)})
+  def    remove(name, args),  do: name   |> GenServer.cast({:remove,    to_mapset(args)})
   def contains?(name, value), do: name   |> GenServer.call({:contains?, value})
 
   defp to_mapset(args), do: args |> MapSet.new()
