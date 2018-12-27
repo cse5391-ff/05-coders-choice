2ba8add516630173114cbc8a9a352c98cfab80a4
Cragsdale1996 <cragsdale@smu.edu>
2018-12-27 14:23:56 +0800

    for some reason the MapSetStore supervisor modification didnt get committed

--- project/map_set_store/lib/map_set_store/dynamic_supervisor.ex
+++ project/map_set_store/lib/map_set_store/dynamic_supervisor.ex
@@ -6,6 +6,10 @@ defmodule MapSetStore.DynamicSupervisor do
     DynamicSupervisor.start_link(__MODULE__, arg, name: __MODULE__)
   end
 
+  def start_child(id, name) do
+    DynamicSupervisor.start_child(__MODULE__, %{id: id, start: {MapSetStore, :start, [name]}})
+  end
+
   def init(_arg) do
     DynamicSupervisor.init(strategy: :one_for_one)
   end
