b13a674a70faa2c4d51a05aa334d9ae4417ab871
cogden14 <cogden14@gmail.com>
2019-01-06 20:39:20 -0600

    updating message form

--- scope/assets/js/app.js
+++ scope/assets/js/app.js
@@ -61,7 +61,7 @@ channel_list.on('click', 'li', function(){
 })
 
 channel.on('shout', payload => {
-     list.append(`<b>${payload.username || 'new_user'}:</b> ${payload.message}<br>`);
+     list.append(`<div class="msg col-md-12"><b>${payload.username || 'new_user'}:</b> ${payload.message}</div>`);
      list.prop({
          scrollTop: list.prop('scrollHeight')
      })
@@ -93,3 +93,18 @@ $(document).ready(function () {
     });
 });
 
+$("input:checkbox").on('click', function() {
+    // in the handler, 'this' refers to the box clicked on
+    var $box = $(this);
+    if ($box.is(":checked")) {
+      // the name of the box is retrieved using the .attr() method
+      // as it is assumed and expected to be immutable
+      var group = "input:checkbox[name='" + $box.attr("name") + "']";
+      // the checked state of the group/box on the other hand will change
+      // and the current value is retrieved using .prop() method
+      $(group).prop("checked", false);
+      $box.prop("checked", true);
+    } else {
+      $box.prop("checked", false);
+    }
+  });
